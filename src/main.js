9aff149dc198d6103571470a723ed1e58c18f153
W.exe <3153311909@qq.com>
2021-06-23 14:41:05 +0800

    引入elementui

--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入element 组件和 ui样式
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 Vue.config.productionTip = false
+
+// 加载使用elementui
+Vue.use(ElementUI)
 
 new Vue({
   router,
---
+++ src/utils/request.js
@@ -0,0 +1,7 @@
+// 基于axios封装请求模块
+
+import axios from 'axios'
+
+axios.create({
+  baseURL: ''
+})
